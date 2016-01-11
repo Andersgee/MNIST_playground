2f87ed907bef4464dac681e9fa9c837c22a65766
andersgee <andersgee@gmail.com>
2016-01-11 18:34:44 +0100

    contrastive divergence

--- Contrastive_Divergence/contrastive_divergence.m
+++ Contrastive_Divergence/contrastive_divergence.m
@@ -1,6 +1,6 @@
 %% Variables
 nrvis=28*28;
-nrhid=49;
+nrhid=10^2;
 v_0=zeros(nrvis,1);
 h_0=zeros(nrhid,1);
 v_1=zeros(nrvis,1);
@@ -11,13 +11,13 @@ images = loadMNISTImages('train-images.idx3-ubyte');
 
 tic
 for n=1:60000
-    v_0=images(:,n);                     %visible units (data vector)
-    h_0=st_sigmoid(W*v_0);               %hidden units (stochastic sigmoid)
-    v_1=st_sigmoid(W'*h_0);              %reconstructed visible units (stochastic sigmoid)
-    h_1=sigmoid(W*v_1);                  %reconstructed hidden units (sigmoid)
-    W=W+0.1*(h_0*v_0'-h_1*v_1');         %Change Weights. Increase when looking at data, decrease when looking at reconstruction.
+    v_0=images(:,n);
+    h_0=st_sigmoid(W*v_0);
+    v_1=st_sigmoid(W'*h_0);
+    h_1=sigmoid(W*v_1);
+    W=W+0.1*(h_0*v_0'-h_1*v_1');
 
-    if (mod(n,200)); continue; end;      %Skip updating plot for most iterations.
+    if (mod(n,300)); continue; end;
     set(Wplot, 'CData',rearrangeW(W)); drawnow;
 end
 display(['time taken: ',num2str(floor(toc)), ' seconds'])
